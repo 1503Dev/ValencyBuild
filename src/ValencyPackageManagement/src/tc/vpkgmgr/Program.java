9150cea335f1987a36ec4ab7d49522dccdac8a2f
HigashitaniYume <higashitaniyume@outlook.com>
2024-11-09 22:46:20 +0800

    多加了几个项目

---
+++ src/ValencyPackageManagement/src/tc/vpkgmgr/Program.java
@@ -0,0 +1,15 @@
+package tc.vpkgmgr;
+
+public class Program {
+	static String github_index = "";
+	static String gitee_index = "";
+	public static void main(String[] args){
+		if (args.length == 0){
+			System.out.println();
+		}
+
+	}
+	static void init_index(){
+		
+	}
+}
